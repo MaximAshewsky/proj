2f9d85ef91ff1ccb8b9374f844b2860891c64c72
Maxim Ashewsky <ashewsky.maxim@yandex.ru>
2025-11-16 15:47:41 +0300

    изменена логика на другую

--- src/main/java/com/stepup/proj/MainApplication.java
+++ src/main/java/com/stepup/proj/MainApplication.java
@@ -12,8 +12,6 @@ public class MainApplication {
         System.out.println("-----");
     }
     public static void doSomething() {
-        System.out.println(0);
-        System.out.println(1);
-        System.out.println(1);
+        System.out.println("Hellow World!");
     }
 }
